b7622811f6429085d677f149b40a48d5a39b0d58
cmlugoce <cmlugoce@syr.edu>
2018-11-08 10:16:16 -0800

    update props

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,7 +7,13 @@ class Weather extends Component {
     render (){
         return (
             <div>
-               Weather
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+               
+               {this.props.temperature && <p>Temperature: {this.props.temperature} F</p>}
+               {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+               {this.props.description && <p>Conditions: {this.props.description}</p>}
+               {this.props.error && <p>{this.props.error}</p>}
+
 
 
             </div>
