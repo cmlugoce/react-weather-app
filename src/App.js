5e1b08e78d68435b8fa4e196325901bba09c23b5
cmlugoce <cmlugoce@syr.edu>
2018-11-08 10:15:02 -0800

    change temp units

--- src/App.js
+++ src/App.js
@@ -21,17 +21,42 @@ class App extends Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
+    if (city && country) {
     console.log(data); 
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    })
+  } else {
+    this.setState({
+      temperature: undefined,
+      city: undefined, 
+      country: undefined, 
+      humidity: undefined, 
+      description: undefined,
+      error: "Please enter the value."
+  })
 }
+  }
 
   render() {
     return (
       <div className="App">
         <Titles/>
         <Form getWeather={this.getWeather}/>
-        <Weather/>
+        <Weather temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+        />
       </div>
     );
   }
