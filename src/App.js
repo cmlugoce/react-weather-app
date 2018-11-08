2958a2c66194e1cb4c721e0667d02a3ad5e32a77
cmlugoce <cmlugoce@syr.edu>
2018-11-08 09:14:09 -0800

    add city, country constant

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Titles from './components/Titles';
 import Form from './components/Form';
@@ -13,7 +12,7 @@ class App extends Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=${API_KEY}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data); 
 }
