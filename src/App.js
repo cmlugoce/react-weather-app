420deef5bb43fa85e3834371ea2805bf6e4c94fc
cmlugoce <cmlugoce@syr.edu>
2018-11-08 19:48:42 -0800

    update files

--- src/App.js
+++ src/App.js
@@ -48,14 +48,14 @@ class App extends Component {
   render() {
     return (
       <div>
-      <div className="wrapper">
-        <div className="main">
-          <div className="container">
+      <div className='wrapper'>
+        
+          <div className="container-fluid">
             <div className="row">
-              <div className="col-xs-6 title-container">
+              <div className="col-xs-5 col-md-6 title-container">
                 <Titles />
               </div>
-              <div className="col-xs-6 form-container">
+              <div className="col-xs-7 col-md-6 form-container">
                 <Form getWeather={this.getWeather} />
                 <Weather 
                   temperature={this.state.temperature} 
@@ -65,7 +65,7 @@ class App extends Component {
                   description={this.state.description}
                   error={this.state.error}
                 />
-              </div>
+              
             </div>
           </div>
         </div>
